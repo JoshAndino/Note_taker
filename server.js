106a27002ba57416d9cf64c2c4f306b23a9e4279
Josh Andino <joshandino1117@gmail.com>
2019-08-22 15:34:21 -0400

    deploy

--- server.js
+++ server.js
@@ -13,45 +13,30 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "notetaker_db"
-  });
-
-  connection.connect(function(err) {
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM notes;", function(err, data) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+      throw err;
     }
-  
-    console.log("connected as id " + connection.threadId);
-  });
-
-  app.get("/", function(req, res) {
-    connection.query("SELECT * FROM notes;", function(err, data) {
-      if (err) {
-        throw err;
-      }
-      res.render("index", {notes: data });
+    res.render("index", { notes: data });
   });
 });
 
 app.post("/new", function(req, res) {
-    connection.query("INSERT INTO notes (title,body) VALUES (?,?)", [req.body.title,req.body.text], function(err, result) {
-        if (err) {
-          throw err;
-        }
-         function newValue(){
-    
-         }
-    
-        res.redirect("/");
-      });
-    });
-    
+  connection.query(
+    "INSERT INTO notes (title,body) VALUES (?,?)",
+    [req.body.title, req.body.text],
+    function(err, result) {
+      if (err) {
+        throw err;
+      }
+      function newValue() {}
+
+      res.redirect("/");
+    }
+  );
+});
+
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
